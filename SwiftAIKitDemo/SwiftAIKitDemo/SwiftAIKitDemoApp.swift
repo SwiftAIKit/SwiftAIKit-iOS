2acc7ab10bfb4d3493d383d313afce477f749063
conversun <conversun@gmail.com>
2025-12-09 19:57:10 +0800

    Add Example project with SwiftUI chat views

---
+++ SwiftAIKitDemo/SwiftAIKitDemo/SwiftAIKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAIKitDemoApp.swift
+//  SwiftAIKitDemo
+//
+//  Created by cyonsun on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAIKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
